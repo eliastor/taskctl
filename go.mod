32895aac8aea5832ae7db7547dd95932e062b5e8
eliastor <eliastor@users.noreply.github.com>
2024-04-30 10:01:31 +0300

    update deprecated code

--- go.mod
+++ go.mod
@@ -26,8 +26,11 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/term v0.19.0 // indirect
+	golang.org/x/text v0.14.0
+	golang.org/x/tools v0.20.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -11,6 +11,8 @@ import (
 	"time"
 
 	"github.com/taskctl/taskctl/pkg/executor"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 
 	"github.com/taskctl/taskctl/pkg/variables"
 
@@ -328,7 +330,7 @@ func (r *TaskRunner) checkTaskCondition(t *task.Task) (bool, error) {
 
 func (r *TaskRunner) storeTaskOutput(t *task.Task) {
 	var envVarName string
-	varName := fmt.Sprintf("Tasks.%s.Output", strings.Title(t.Name))
+	varName := fmt.Sprintf("Tasks.%s.Output", cases.Title(language.English).String(t.Name))
 
 	if t.ExportAs == "" {
 		envVarName = fmt.Sprintf("%s_OUTPUT", strings.ToUpper(t.Name))
